5b2ac74594de86b72dbed8b8b5867e2b77f89a5b
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-11-14 16:10:49 +0500

    Update Home.tsx

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <section id="home" className="home-section">
       <div className="profile-container">
         <Image src="/Hina M.Rafiq.jpeg" alt="Hina Muhammad Rafiq" width={200} height={200} className="profile-img" />
-        <h1>Hi, I'm <span>Hina Muhammad Rafiq</span></h1>
+        <h1>Hi, I am <span>Hina Muhammad Rafiq</span></h1>
         <p>Passionate about exploring and embracing new technologies particularly in the world of <b> GenAI </b>. With expertise in <i>HTML, CSS, JavaScript and TypeScript</i>. I transform ideas into visually stunning and responsive web applications that captivate and engage.
         </p>
       </div>
