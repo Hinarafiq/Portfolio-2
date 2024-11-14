0e92c85cad8e8f4ca82b6b586cbeb5590df7a576
Hinarafiq <161075228+Hinarafiq@users.noreply.github.com>
2024-11-14 16:10:25 +0500

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,12 +3,12 @@ export default function About() {
       <section id="about" className="about-section">
         <h2>About Me</h2>
         <p>
-        <strong>Hello! I'm Hina Muhammad Rafiq,</strong> a dedicated and passionate 
+        <strong>Hello! I am Hina Muhammad Rafiq,</strong> a dedicated and passionate 
         Web Developer with a capibility for building interactive and visually engaging 
         web applications. My journey in web development began with a strong foundation
-        in HTML, CSS,Tailwind CSS , JavaScript and TypeScript, and I’m continuously
+        in HTML, CSS,Tailwind CSS , JavaScript and TypeScript, and I am continuously
         expanding my skill set to bring innovative solutions to life. Currently, 
-        I'm diving into Next.js to deepen my knowledge and explore advanced 
+        I am diving into Next.js to deepen my knowledge and explore advanced 
         front-end possibilities.
         </p>
       </section>
